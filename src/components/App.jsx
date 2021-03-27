27753e48453c31024c7601652abc6daede82dea2
lucasbivar <lucasbivarfonseca@gmail.com>
2021-03-27 16:47:56 -0300

    fix gradient bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,9 @@ import About from './About';
 import Testemonials from './Testemonials';
 import Footer from "./Footer";
 import Button from "./Button";
+import bgDark from "../assets/images/bg-curvy-desktop.svg";
+import bgLight from "../assets/images/bg-curvy-desktop_light.svg";
+
 
 export default function App() {
 
@@ -20,25 +23,21 @@ export default function App() {
       document.documentElement.style.setProperty('--featuresBackground',"hsl(218, 28%, 13%)");
       document.documentElement.style.setProperty('--testemonialsBackground',"hsl(218, 28%, 13%)");
       document.documentElement.style.setProperty('--aboutBackground',"hsl(218, 28%, 13%)");
-      // document.documentElement.style.setProperty('--gradient','url("../assets/images/bg-curvy-desktop.svg")');
       document.documentElement.style.setProperty('--cardBackground',"hsl(219, 30%, 18%)");
       document.documentElement.style.setProperty('--footerBackground',"hsl(216, 53%, 9%)");    
       document.documentElement.style.setProperty('--btn-theme',"#fdfdfd");
-      // document.getElementById("title").style.backgroundImage = 'url(../assets/images/bg-curvy-desktop.svg)';
-      
-      
-
+      document.getElementById("title").style.backgroundImage = "url("+ bgDark+")";
     }else{
       document.documentElement.style.setProperty('--textColor',"hsl(218, 28%, 13%)");
       document.documentElement.style.setProperty('--mainBackground',"#EBEEF0");
       document.documentElement.style.setProperty('--featuresBackground',"#FFFFFF");
       document.documentElement.style.setProperty('--testemonialsBackground',"#FFFFFF");
       document.documentElement.style.setProperty('--aboutBackground',"#FFFFFF");
-      // document.documentElement.style.setProperty('--gradient','url(../assets/images/bg-curvy-desktop_light.svg)');
       document.documentElement.style.setProperty('--cardBackground',"#EFF1F2");    
       document.documentElement.style.setProperty('--footerBackground',"#EBEEF0");    
-      document.documentElement.style.setProperty('--btn-theme',"hsl(217, 28%, 15%)");   
-      // document.getElementById("title").style.backgroundImage = 'url(../assets/images/bg-curvy-desktop_light.svg)';
+      document.documentElement.style.setProperty('--btn-theme',"hsl(217, 28%, 15%)");  
+      document.getElementById("title").style.backgroundImage = "url("+ bgLight+")";
+
     }
   }
 
